0a96662dae1bd156cf5066532cf3465748f7d62e
Abhishek Samanta <abhisheksamanta.apps@gmail.com>
2026-06-27 00:21:58 +0200

    Initial Commit

---
+++ pdfpagearranger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pdfpagearranger
+//
+//  Created by Abhishek Samanta on 27.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pdfpagearranger/pdfpagearrangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pdfpagearrangerApp.swift
+//  pdfpagearranger
+//
+//  Created by Abhishek Samanta on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct pdfpagearrangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
